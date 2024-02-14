b9fecd8c5590aebb93c8368ab83086b394056477
Glenn <islandcreation@outlook.com>
2024-02-14 15:37:36 -0500

    updated Footer and MainPage

--- ghi/app/src/Footer.jsx
+++ ghi/app/src/Footer.jsx
@@ -7,11 +7,11 @@ const Footer = () => {
             <div className="bg-dark text-center text-white">
                 <div className="container p-4 pb-0">
                     <section className="mb-4">
-                        CarCar is the premiere solution for automobile sales and services.
+                        Hawaii Imports is the premiere solution for automobile sales and services.
                     </section>
                 </div>
                 <div className="text-center p-3" style={{backgroundColor: 'rbga(0, 0, 0, 0.2)'}}>
-                    Copyright &copy; CarCar 2024
+                    Copyright &copy; Hawaii Imports 2024
                 </div>
             </div>
         </footer>
