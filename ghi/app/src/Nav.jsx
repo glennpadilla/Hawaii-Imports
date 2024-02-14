9e228be1df1f2b39b3a3ff8a4bc0d3971ee51327
Glenn <islandcreation@outlook.com>
2024-02-14 17:09:13 -0500

    updated Nav.jsx

--- ghi/app/src/Nav.jsx
+++ ghi/app/src/Nav.jsx
@@ -11,7 +11,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
+        <NavLink className="navbar-brand" to="/">Hawaii Imports</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
